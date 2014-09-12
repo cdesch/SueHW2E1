2c1f86cee1cb9c7bc0526face8c2072ef698d60b
Chris <cdesch@gmail.com>
2014-09-12 19:37:55 -0400

    Assignment Framework
    Signed-off-by: Chris <cdesch@gmail.com>

--- SueHW2E1/main.cpp
+++ SueHW2E1/main.cpp
@@ -6,13 +6,248 @@
 //  Copyright (c) 2014 KickinEspresso. All rights reserved.
 //
 
+//
+//  main.cpp
+//  Lab 2 part 1
+//
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream>
 
-int main(int argc, const char * argv[])
-{
+using namespace std;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+//This function converts a string to an integer
+//It returns a variable of type int
+int converStringToInt(string myString){
+    //atoi() isn't something you would normally know about it
+    //Documentation link http://www.cplusplus.com/reference/cstdlib/atoi/
+    return atoi(myString.c_str());
+}
+//This function converts a string to an long
+//It returns a variable of type long
+long convertStringToLong(string myString){
+    //Documentation Link http://www.cplusplus.com/reference/cstdlib/atol/
+    return atol(myString.c_str());
+}
+
+class Date {
+    
+private:
+    int year;
+    int month;
+    int day;
+    
+public:
+    //Constructor
+    Date(); //Default Constructor
+    Date(int y, int m, int d); //This is the constructor
+    Date(string d); //Overloaded Constructor with accepting long date format
+    //Deconstructor
+    ~Date();
+    
+    //Member functions//
+    void printInfo();
+    
+    //GETTERS//
+    int getYear();
+    int getMonth();
+    int getDay();
+    
+    //SETTERS//
+    void setYear(int y);
+    void setMonth(int m);
+    void setDay(int d);
+};
+
+//Default Constructor
+Date::Date(){
+    this->year =  0;
+    this->month =  0;
+    this->day =  0;
+}
+
+//Constructor with params
+Date::Date(int y, int m, int d){
+    year = y;
+    month = m;
+    day = d;
+}
+
+Date::Date(string d){
+    
+    //Change from string to a long
+    long dateValue = convertStringToLong(d);
+    
+    //break the dateValue into Year Month Day
+    //Hint use Modulus (%)
+    // dateValue % 10000
+    //TODO: Implement
+    
+}
+
+//Deconstructor
+Date::~Date(){
+}
+
+//Member Functions//
+void Date::printInfo(){
+    //FIX ME: Print in "YYYYMMDD" Format
+    //                  20140912 //Watch for the Leading  zero
+    printf("Year: %d \n", year);
+    printf("Month: %d \n", month);
+    printf("Day: %d \n", day);
+    
+    //Hint: Print year, If month is less than 10, print a 0, then number.... if not print number
+}
+
+//GETTERS//
+int Date::getYear(){
+    return year;
+}
+int Date::getMonth(){
+    return month;
+}
+int Date::getDay(){
+    return day;
+}
+
+//SETTERS//
+void Date::setYear(int y){
+    year = y;
+}
+void Date::setMonth(int m){
+    month = m;
+}
+void Date::setDay(int d){
+    day = d;
+}
+
+
+
+//**************************
+//  Class Person
+//**************************
+class Person {
+    
+private:
+    string lastName;
+    string firstName;
+    long ssn;
+    Date birthday;
+public:
+
+    //Constructor
+    Person(string last, string first, long s, Date b);
+    ~Person(); //Deconstructor
+    
+    //Member Functions//
+    void printName();
+    void printInfo(); //Print Name (first and last), SSN, Birthdate
+    long age();     //Calculate the age of the person based on the birthdate
+ 
+    //Setters
+    //TODO Set/Get: lastName
+    //TODO Set/Get: firstName
+    
+    //TODO Set/Get: ssn
+    //TODO Set/Get: Date
+    //TODO setFullName(string name)
+    
+    //Getters are after this
+    //TODO getFullName()
+    
+};
+
+//Constructor with params
+Person::Person(string last, string first, long s, Date b){
+    lastName = last;
+    firstName = first;
+    //TODO: assign ssn;
+    birthday = b;
+    
+}
+
+//Deconstructor
+Person::~Person(){
+}
+
+//Member Functions//
+//Prints full name
+void Person::printName(){
+    printf("%s %s \n", firstName.c_str(), lastName.c_str());
+}
+
+//TODO void Person::printInfo(){}
+
+long Person::age(){
+    //This function computes the age to the nearest year.
+    //int age = 2014 - year;
+    //Hint: birthday.getYear();
+    return 0; //Implement
+}
+
+//TODO:Setters Implementation
+
+//TODO:Getters Implementation
+
+//Notes:
+//I know I need to read in the file and get the YYYYMMDD string and then break it up into integers year=YYYY, month=MM, and day=DD
+//Need to read file and also need to modify this code, I think
+
+//ReadFile reads database and creates the objects
+
+
+vector <Person> readFile(string filename){
+    vector <Person> people;
+
+    //Read each line, create object, add to people Vector  - Loop
+    //use .push_back(MYOBJECT) on people Vector EX: people.push_back(MYOBJECT)
+    //HINT: From Lab1E4 - instead of creating a book object, you will create a Person Object.... instead of a PersonStruct Object, you will create a date object.
+
+    return people;
+}
+
+void labTwo(){
+    cout << endl;
+    cout <<"***************************************************"<< endl;
+    cout << "  Reading file" << endl;
+    cout <<"***************************************************"<< endl;
+    vector <Person> people = readFile("/Users/susanchang/Desktop/database1.txt");
+}
+
+int main(int argc, const char * argv[]){
+    cout << "Lab 2, Part 1 \n";
+    labTwo();
     return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
