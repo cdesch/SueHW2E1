07cb8a28932dcbe6bd3b655aee549a3819952438
Chris <cdesch@gmail.com>
2014-09-12 15:39:00 -0400

    Add Person
    Signed-off-by: Chris <cdesch@gmail.com>

---
+++ SueHW2E1/person.cpp
@@ -0,0 +1,9 @@
+//
+//  person.cpp
+//  SueHW2E1
+//
+//  Created by cj on 9/11/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#include "person.h"
---
+++ SueHW2E1/person.h
@@ -0,0 +1,14 @@
+//
+//  person.h
+//  SueHW2E1
+//
+//  Created by cj on 9/11/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#ifndef SueHW2E1_person_h
+#define SueHW2E1_person_h
+
+
+
+#endif
