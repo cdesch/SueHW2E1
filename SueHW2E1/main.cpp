420171b6ac0a411a5d84c1cd7bfc9a3f211e284b
Chris <cdesch@gmail.com>
2014-09-16 11:47:06 -0400

    Updating Change
    Signed-off-by: Chris <cdesch@gmail.com>

--- SueHW2E1/main.cpp
+++ SueHW2E1/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Lab 2 part 1
+//  Lab 2 part 1 and 2
 //
 
 #include <iostream>
@@ -22,9 +22,29 @@ int converStringToInt(string myString){
 }
 //This function converts a string to an long
 //It returns a variable of type long
+//This function assumes that the string being converted is NOT a zero!!
 long convertStringToLong(string myString){
     //Documentation Link http://www.cplusplus.com/reference/cstdlib/atol/
-    return atol(myString.c_str());
+    long convertedNumber;
+    convertedNumber = atol(myString.c_str());
+    //Check if conversion failed
+    if (convertedNumber == 0){
+        cout << "***Error: Conversion to long Failed. BOM Unicode bytes detected. " << endl;
+        cout << "***Remove Bytes!! " << endl;
+        cout << "***Error Reading File. Remove Unicode Bytes from first line" << endl;
+        for (int i = 0; i < myString.length(); i++){
+            cout << i << ": " << myString[i] << " ";
+            //If it is not a digit, remove it
+            if (!isdigit(myString[i])){
+                cout << " <--  ***** Not a integer *****" ;
+            }
+            cout << endl;
+        }
+        throw  "***Error Reading File. Remove Unicode Bytes from first line";
+        string newString = myString.substr(3);
+        convertedNumber = atol(newString.c_str());
+    }
+    return convertedNumber;
 }
 
 class Date {
@@ -79,10 +99,6 @@ Date::Date(string d){
     year = int(dateValue/10000);
     month = (dateValue/100) % 100;
     day = dateValue % 100;
-    
-    //cout << "Year " << year << " " << " Month ";
-    //if (month < 10) cout << "0" << month << "Day ";
-    //if (day < 10) cout << "0" << day << endl;
 }
 
 //Deconstructor
@@ -143,22 +159,19 @@ public:
     void printInfo(); //Print Name (first and last), SSN, Birthdate
     long age();     //Calculate the age of the person based on the birthdate
     
-    //Setters
+    //Setters and Getters
     //TODO Set/Get: lastName
     //TODO Set/Get: firstName
     //TODO Set/Get: ssn
     //TODO Set/Get: Date
     //TODO setFullName(string name)
     
-    //Getters are after this
-    //TODO getFullName()
     //GETTERS//
     long getSsn();
     string getFirstName();
     string getLastName();
     Date getBirthday();
     string getFullName(string fullname);
-
     
     //SETTERS//
     void setSsn(long s);
@@ -167,7 +180,6 @@ public:
     void setBirthday(Date b);      //Overloaded!!
     void setBirthday(string b);    //Overloaded!!
     void setFullName(string fullname);
-    
 };
 
 //Constructor with params
@@ -188,14 +200,12 @@ void Person::printName(){
     printf("%s %s \n", firstName.c_str(), lastName.c_str());
 }
 
-//TODO void Person::printInfo(){}
 void Person::printInfo(){
     printf("SSN: %ld \n", ssn);
     printf("First Name: %s \n", firstName.c_str());
     printf("Last Name: %s \n", lastName.c_str());
     printf("Birthdate (YYYYMMDD): ");
-    birthday.printInfo();
-    //Birthdate (YYYYMMDD): 20140914
+    birthday.printInfo();  //Birthdate (YYYYMMDD): 20140914
 }
 
 long Person::age(){
@@ -232,29 +242,19 @@ void Person::setLastName(string last){
 
 //Overloading -- two different means to the same end.
 void Person::setBirthday(Date b){
-     birthday = b;
+    birthday = b;
 }
 void Person::setBirthday(string b){
-     birthday = Date(b);
+    birthday = Date(b);
 }
-
-
-//Notes:
-//I know I need to read in the file and get the YYYYMMDD string and then break it up into integers year=YYYY, month=MM, and day=DD
-//Need to read file and also need to modify this code, I think
-
-
 
 //ReadFile reads database and creates the objects
 vector <Person> readFile(string filename){
     vector <Person> people; //Vector of TYPE Person named 'people'
-    //Read each line, create object, add to people Vector  - Loop
-    //use .push_back(MYOBJECT) on people Vector EX: people.push_back(MYOBJECT)
-    //HINT: From Lab1E4 - instead of creating a book object, you will create a Person Object.... instead of a PersonStruct Object, you will create a date object.
     
     ifstream infile(filename); //Open the file
     string str; //Declares a string and is used for temporary storage
-
+    
 	if (infile.is_open()){
 		while (getline(infile,str, '\r')){
             string ssnString;
@@ -280,27 +280,22 @@ vector <Person> readFile(string filename){
             if (getline(stringStream, token, ' ')){
                 dateString = token;
             }
-            
-            
+
             //Date Object
             Date birthdayVariable(dateString); //Creates a new date object
-            
             //Person     Person(long s, string first, string last, Date b);
-            long ssn = convertStringToLong(ssnString);
+            long ssn =  convertStringToLong(ssnString);
+            
             Person personVariable(ssn, firstName, lastName, birthdayVariable);
             people.push_back(personVariable);
-            
 		}
 		infile.close();
-        
 	}
 	else{
 		cout << "Unable to open file" << endl << endl;
 	}
-    
     return people;
 }
-
 
 void printPeopleVector(vector<Person> people){
     //Check our work by printing the information for the vector of people objects
@@ -311,7 +306,6 @@ void printPeopleVector(vector<Person> people){
     }
 }
 
-
 void printAgeOfPeopleVector(vector<Person> people){
     //Check our work by printing the information for the vector of people objects
     for(long i = 0; i < people.size(); i++){
@@ -319,19 +313,15 @@ void printAgeOfPeopleVector(vector<Person> people){
     }
 }
 
-
-
-//TODO: change 'vector <int>' to 'vector <Person>' because we are sorting people.
-//TODO: When comparing people, call the .age()  -- Ex: myArray[i].age();  //Only during comparison
-vector <int> bubbleSort(vector <int> myArray){
+//Bubble Sort
+vector <Person> bubbleSort(vector <Person> myArray){
     //Loop through
-    //Compare what ? if larger : swap ...
+    //Compare and swap ...
     for(int loop = 1; loop <myArray.size(); loop++ ){
         for(int i = 1 ; i <myArray.size(); i++){
-            
-            if(myArray[i-1] > myArray[i]){
+            if(myArray[i-1].age() > myArray[i].age()){
                 //Swap
-                int temp = myArray[i];
+                Person temp = myArray[i];
                 myArray[i] = myArray[i-1];
                 myArray[i-1] = temp;
             }
@@ -340,12 +330,13 @@ vector <int> bubbleSort(vector <int> myArray){
     return myArray;
 }
 
-vector <int> insertionSort(vector <int> myArray){
+//Insertion Sort
+vector <Person> insertionSort(vector <Person> myArray){
     for(int i = 0; i <= myArray.size(); i++ ){
         for(int j = i - 1 ; j > 0; j--){
-            if(myArray[j-1] > myArray[j]){
+            if(myArray[j-1].age() > myArray[j].age()){
                 //Swap
-                int temp = myArray[j];
+                Person temp = myArray[j];
                 myArray[j] = myArray[j-1];
                 myArray[j-1] = temp;
             }
@@ -354,33 +345,35 @@ vector <int> insertionSort(vector <int> myArray){
     return myArray;
 }
 
-vector <int> selectionSort(vector <int> myArray){
+//Selection Sort
+vector <Person> selectionSort(vector <Person> myArray){
     int i, j, smallestNow;
     for(i=0; i<myArray.size()-1; i++){
         smallestNow = i;
         for(j=i+1; j<myArray.size(); j++){
-            if(myArray[smallestNow] > myArray[j]) smallestNow=j;
+            if(myArray[smallestNow].age() > myArray[j].age()) smallestNow=j;
         }
-        int temp = myArray[i];
+        Person temp = myArray[i];
         myArray[i] = myArray[smallestNow];
         myArray[smallestNow] = temp;
     }
     return myArray;
 }
 
-vector <int> shakerSort(vector <int> myArray){
+//Shaker Sort
+vector <Person> shakerSort(vector <Person> myArray){
     for(int i=0; i<=myArray.size()/2; i++){
         for(int j=i; j<(myArray.size())-(i+1); j++){
-            if(myArray[j] > myArray[j+1]){
+            if(myArray[j].age() > myArray[j+1].age()){
                 //Swap
-                int temp = myArray[j];
+                Person temp = myArray[j];
                 myArray[j] = myArray[j+1];
                 myArray[j+1] = temp;
             }
         }
         for(int j= int(myArray.size()-(2+i)); j > i; j--){
-            if(myArray[j] < myArray[j-1]){
-                int temp = myArray[j];
+            if(myArray[j].age() < myArray[j-1].age()){
+                Person temp = myArray[j];
                 myArray[j] = myArray[j-1];
                 myArray[j-1]=temp;
             }
@@ -389,38 +382,66 @@ vector <int> shakerSort(vector <int> myArray){
     return myArray;
 }
 
+double getCPUTime(void) {
+    struct timeval tv;
+    struct rusage ru;
+    getrusage(RUSAGE_SELF, &ru);
+    tv = ru.ru_utime;
+    double t = (double)tv.tv_sec + (double)tv.tv_usec/1000000.0;
+    tv = ru.ru_stime;
+    t += (double)tv.tv_sec + (double)tv.tv_usec/1000000.0;
+    return t;
+}
+
 void labTwo(){
+    double startTime, endTime, totalTime;
     cout << endl;
     cout <<"***************************************************"<< endl;
     cout << "  Reading file" << endl;
     cout <<"***************************************************"<< endl;
-    vector <Person> peopleVectorDB1 = readFile("/Users/cj/Desktop/database1.txt");
+    
+    vector <Person> peopleVectorDB1 = readFile("/Users/cj/Desktop/database2.txt");
     
     printPeopleVector(peopleVectorDB1);
     
-    //Timer Start
-    //vector <Person> bubbleSortedPeopleVector = bubbleSort(peopleVectorDB1);
-    //Timer Stop
+    cout << "Timing Output for Four Sort Algorithms: \n \n";
+    cout << "Bubble Sort: " << endl;
+    startTime = getCPUTime();
+    vector <Person> bubbleSortedPeopleVector = bubbleSort(peopleVectorDB1);
+    endTime = getCPUTime();
+    totalTime = endTime - startTime;
     //printPeopleVector(bubbleSortedPeopleVector);
-    //Print timer Results
+    cout << "Total time: " << totalTime << " = End time: " << endTime << " - Start time: " << startTime << endl;
     
+    cout << endl;
+    cout << "Insertion Sort: " << endl;
+    startTime = getCPUTime();
+    vector <Person> insertionSortedPeopleVector = insertionSort(peopleVectorDB1);
+    endTime = getCPUTime();
+    totalTime = endTime - startTime;
+    //printPeopleVector(insertionSortedPeopleVector);
+    cout << "Total time: " << totalTime << " = End time: " << endTime << " - Start time: " << startTime << endl;
     
-    //vector <Person> selectionSortedPeopleVector = selectionSort(peopleVectorDB1);
+    cout << endl;
+    cout << "Selection Sort: " << endl;
+    startTime = getCPUTime();
+    vector <Person> selectionSortedPeopleVector = selectionSort(peopleVectorDB1);
+    endTime = getCPUTime();
+    totalTime = endTime - startTime;
     //printPeopleVector(selectionSortedPeopleVector);
+    cout << "Total time: " << totalTime << " = End time: " << endTime << " - Start time: " << startTime << endl;
     
-    
-    //printAgeOfPeopleVector(peopleVectorDB1);
-    
-    
+    cout << endl;
+    cout << "Shaker Sort: " << endl;
+    startTime = getCPUTime();
+    vector <Person> shakerSortedPeopleVector = shakerSort(peopleVectorDB1);
+    endTime = getCPUTime();
+    totalTime = endTime - startTime;
+    cout <<endl;
+    printPeopleVector(shakerSortedPeopleVector);
+    //printPeopleVector(shakerSortedPeopleVector);
+    cout << "Total time: " << totalTime << " = End time: " << endTime << " - Start time: " << startTime << endl;
 }
-
-
-//Test Prep List
-// 1 sort of the 4 - comparison sort analysis.
-// variable scope
-// function based question (parameters)
-// Class based question
-//
 
 int main(int argc, const char * argv[]){
     cout << "Lab 2, Part 1 \n";
